697716eef06a62a96e4b8ace17644de12eaf8605
vkcosta <vito.knupp@gmail.com>
2025-06-24 18:02:49 +0100

    CRiado a tela de resume.

--- src/app/components/TimelineSection.tsx
+++ src/app/components/TimelineSection.tsx
@@ -1,4 +1,8 @@
-interface InputTimelineSection {
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export interface InputTimelineSection {
+    icon?: IconProp
     title: string;
     items: {
         title: string,
@@ -7,19 +11,18 @@ interface InputTimelineSection {
     }[];
 }
 
-const TimelineSection = ({ title, items }: InputTimelineSection) => {
-
+const TimelineSection = ({ title, items, icon }: InputTimelineSection) => {
     return (
         <section className="timeline-section text-white py-8 px-4">
             <h2 className="text-2xl font-bold mb-6 flex items-center gap-2">
-                <span className="bg-yellow-500 p-2 rounded-full text-black">ícone aqui</span>
+                {icon && <span className="bg-[lightblue] p-2 rounded-full text-[blue]"><FontAwesomeIcon icon={icon} /></span>}
                 {title}
             </h2>
 
-            <div className="relative border-l border-gray-700 pl-6">
+            <div className="relative border-l border-[blue] pl-6">
                 {items.map((item, index) => (
                     <div key={index} className="mb-10 relative">
-                        <div className="absolute -left-3 top-1 w-4 h4 bg-yellow-500 rounded-full border-2 border-gray-900"></div>
+                        <div className="absolute -left-8 top-1 w-4 h-4 bg-[blue] rounded-full border-4 border-[rgba(255,255,255,0.2)]"></div>
                         <h3 className="text-lg font-semibold">{item.title}</h3>
                         <span className="text-sm text-gray-400">{item.subtitle}</span>
                         <p className="mt-2 text-gray-300">{item.description}</p>
--- src/app/resume/Resume.tsx
+++ src/app/resume/Resume.tsx
@@ -1,7 +1,52 @@
+import { faBriefcase, faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+import TimelineSection, { InputTimelineSection } from "../components/TimelineSection";
+
+
+
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
+  const resume: InputTimelineSection[] = [
+    {
+      icon: faGraduationCap,
+      title: "Education",
+      items: [
+        {
+          title: 'Bacharelado em Sistemas de Informação',
+          subtitle: '2015 - 2019',
+          description: 'Centro Universitário Católico de Vitória'
+        },
+        {
+          title: 'Curso Técnico em Informática',
+          subtitle: '2011 - 2012',
+          description: 'Centro Estadual de Educação Técnica Vasco Coutinho'
+        },
+      ]
+    },
+    {
+      icon: faBriefcase,
+      title: "Experience",
+      items: [
+        {
+          title: 'Microkids — Desenvolvedor de Software (Freelancer)',
+          subtitle: '2023 - 2024',
+          description: `Desenvolvimento de uma aplicação comercial voltada para educação infantil, nas plataformas web (Angular) e desktop (Electron).
+          Implementação de um editor gráfico interativo com Fabric.js, permitindo a manipulação dinâmica de elementos visuais em atividades educacionais.
+          Contribuição para melhorias de interface (UI/UX) e desempenho da aplicação.`
+        },
+        {
+          title: 'EcoSoft — Analista de Sistemas',
+          subtitle: '2018 - 2025',
+          description: `Responsável pela análise de requisitos, modelagem de dados, desenvolvimento de APIs RESTful e interfaces web utilizando TypeScript, Angular, Node.js e PostgreSQL. Atuei na implementação de novas funcionalidades, correção de bugs, refatoração de código legado e melhoria contínua da performance dos sistemas. Participei de revisões de código, testes automatizados e integrações com serviços externos. Colaborei com equipes multidisciplinares em metodologias ágeis, entregando soluções voltadas à gestão e análise de dados ambientais.`
+        },
+      ]
+    }
+  ];
+
+  return resume.map(({ title, items, icon }, index) => (
+    <div key={index}>
+      <TimelineSection title={title} items={items} icon={icon}></TimelineSection>
+      <p></p>
+    </div>
+  ))
 }
 
 export default Resume;
